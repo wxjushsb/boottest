fd81aa893814b109a52a9c4970c604a98538fc4d
Your Name <email@example.com>
2022-12-05 18:24:22 +0800

    Initial commit

---
+++ src/main/java/com/example/springboottesr/SpringBoottesrApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottesr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoottesrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoottesrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottesr/SpringBoottesrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottesr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoottesrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
